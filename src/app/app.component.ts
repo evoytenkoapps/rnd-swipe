4a8aaa0986ef5d2564595c7fa79d2236ac47e2c6
Евгений Войтенко <e.voytenko@tinkoff.ru>
2023-08-23 18:56:00 +0300

    fix bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,11 @@ export class AppComponent {
   title = 'rnd-swipe';
 
   constructor(private readonly sheets: TuiSheetDialogService) {
-    // this.sheets
-    //   .open('Content', {
-    //     label: 'Heading',
-    //     offset: 48,
-    //   })
-    //   .subscribe();
+    this.sheets
+      .open('Content', {
+        label: 'Heading',
+        offset: 48,
+      })
+      .subscribe();
   }
 }
