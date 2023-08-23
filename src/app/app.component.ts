ec417e6879d52d883225c6b9ac61b2ab561fb88f
Евгений Войтенко <e.voytenko@tinkoff.ru>
2023-08-23 17:38:11 +0300

    add TuiSheetDialogService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { TuiSheetDialogService } from '@taiga-ui/addon-mobile';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
   title = 'rnd-swipe';
+
+  constructor(private readonly sheets: TuiSheetDialogService) {
+    this.sheets
+      .open('Content', {
+        label: 'Heading',
+        offset: 48,
+      })
+      .subscribe();
+  }
 }
