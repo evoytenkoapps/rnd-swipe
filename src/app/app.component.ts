2977bc7bdf8ece10a19238c8b8e99fe0228f465d
Евгений Войтенко <e.voytenko@tinkoff.ru>
2023-08-24 19:37:47 +0300

    add templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {
   AfterViewInit,
   Component,
+  ElementRef,
   TemplateRef,
   ViewChild,
 } from '@angular/core';
@@ -15,18 +16,34 @@ export class AppComponent implements AfterViewInit {
   @ViewChild('savedBankTemplate', { read: TemplateRef })
   sayHelloTemplate: TemplateRef<any>;
 
+  @ViewChild('banksTemplate', { read: ElementRef })
+  banksTemplate: ElementRef;
+
   isSavedOpened = false;
   isBanksOpened = false;
+
+  templateOptions = { stops: [] };
 
   constructor(private readonly sheets: TuiSheetDialogService) {}
 
   ngAfterViewInit(): void {
-    // this.sheets
-    //   .open(this.sayHelloTemplate, {
-    //     closeable: true,
-    //     offset: 0,
-    //   })
-    //   .subscribe();
+    this.sheets
+      .open(this.sayHelloTemplate, {
+        closeable: true,
+        offset: 0,
+        stops: ['10rem'],
+      })
+      .subscribe({
+        next(x) {
+          console.log('got value ' + x);
+        },
+        error(err) {
+          console.error('something wrong occurred: ' + err);
+        },
+        complete() {
+          console.log('done');
+        },
+      });
   }
 
   onShowBanks() {
@@ -40,5 +57,14 @@ export class AppComponent implements AfterViewInit {
 
   onPayApp() {
     this.isSavedOpened = false;
+  }
+
+  onInputClick() {
+    // @ts-ignore
+    this.templateOptions = { stops: ['100rem', '100rem'] };
+    console.log(this.templateOptions);
+
+    this.banksTemplate.nativeElement.style.transition = 'height 0.2s ease-out';
+    this.banksTemplate.nativeElement.style.height = '100vh';
   }
 }
