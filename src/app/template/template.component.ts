85284518db60dfa3ca1eb4b1b3d51cb6a3600bad
Евгений Войтенко <e.voytenko@tinkoff.ru>
2023-08-25 17:28:23 +0300

    add templates

--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-template',
   templateUrl: './template.component.html',
   styleUrls: ['./template.component.less'],
 })
-export class TemplateComponent {}
+export class TemplateComponent {
+  @Input() userName: string;
+}
