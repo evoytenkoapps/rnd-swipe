19bdf2f0a94a5d423ff1c5689ab39f36ef4c7375
Евгений Войтенко <e.voytenko@tinkoff.ru>
2023-08-28 17:07:16 +0300

    del stops

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,7 +43,6 @@ export class AppComponent implements AfterViewInit {
         switchMap(() =>
           this.sheets.open(this.banksTemplate, {
             closeable: true,
-            stops: ['500px'], // 280 + 56 + 24
           }),
         ),
       )
