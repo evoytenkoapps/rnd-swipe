b410631950ef3f56b516a93269d5350e865d8835
Евгений Войтенко <e.voytenko@tinkoff.ru>
2023-08-29 13:13:36 +0300

    fixed size

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,3 @@ import { AppModule } from './app/app.module';
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
-
-/*
-     На мобильных браузерах в высоту вьюпорта вставляется адерсная строка, что мешает верстке
-     https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
-    */
-const mobileViewPortHeight = window.innerHeight * 0.01;
-const viewPortPropName = 'pf-mobileViewPortHeight';
-
-addCssProperty(viewPortPropName, mobileViewPortHeight);
-
-// Чтобы не лагала верстка в мобильном браузере когда прячется адресная строка
-window.addEventListener('resize', () => {
-  addCssProperty(viewPortPropName, mobileViewPortHeight);
-});
-
-function addCssProperty(name: string, px: number) {
-  document.documentElement.style.setProperty(`--${name}`, `${px}px`);
-}
